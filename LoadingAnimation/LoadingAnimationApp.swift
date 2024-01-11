61cf34d7b81c466b25ade1f0fd5b273d1a778518
doris <doris070714@gmail.com>
2024-01-11 17:52:31 +0800

    Initial Commit

---
+++ LoadingAnimation/LoadingAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingAnimationApp.swift
+//  LoadingAnimation
+//
+//  Created by Doris Wen on 2024/1/11.
+//
+
+import SwiftUI
+
+@main
+struct LoadingAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
